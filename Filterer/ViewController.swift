1c8b52786dbd2deedd0f600280a0cf265772126b
shmakova <shmakova-nastya@yandex.ru>
2016-01-18 22:44:35 +0600

    Initial Commit

---
+++ Filterer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Filterer
+//
+//  Created by Anastasia Shmakova on 18.01.16.
+//  Copyright © 2016 Shmakova Anastasia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
