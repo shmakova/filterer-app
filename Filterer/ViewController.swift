80dc0116c67574cd378878bb1808ee843f7a4481
shmakova <shmakova-nastya@yandex.ru>
2016-01-19 23:52:53 +0600

    auto layout

--- Filterer/ViewController.swift
+++ Filterer/ViewController.swift
@@ -30,7 +30,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+
         let image = UIImage(named: "sample")!
         
         let rgbaImage = RGBAImage(image: image)!
